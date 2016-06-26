92890bb0c478bb0c089ec273e6b12def0a869d83
Spyros Maniatopoulos <spmaniato@gmail.com>
2016-06-26 16:15:39 -0500

    Modify app. Remove binary.

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <iostream>
 
-int main (int argc, char **argv) 
+int main (int argc, char **argv)
 {
-	printf("Hello, world!\n");
-	return 0;
+    std::cout << "Hello, Spyros!" << std::endl;
+    return 0;
 }
